d490062d1d25e2759256788811df8e5a3a5518e2
liuchunyueh <liuchunyueh@gmail.com>
2018-05-20 19:27:31 +0800

    setup relation among users, restaurants, and likes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,8 +8,12 @@ class Restaurant < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 
+  #「餐廳擁有很多喜歡它的使用者」的多對多關係
+    has_many :likes, dependet: :destroy
+    has_many :liked_users, through: :likes, source: :user
+
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
-  
+
 end
