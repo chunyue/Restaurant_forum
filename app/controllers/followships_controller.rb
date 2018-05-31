19bf65af8ef6c3643b0c54ef437e5efeecc471eb
liuchunyueh <liuchunyueh@gmail.com>
2018-06-01 00:21:22 +0800

    add cteate action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,16 @@
 class FollowshipsController < ApplicationController
+
+  def create
+    @user = User.find(params[:format])
+      @user.followships.new(user_id: current_user.id, following_id: @user)
+    if @user.save
+        redirect_back(fallback_location: root_path) 
+        flash[:notice] = "Follow was successful created"
+    else
+      redirect_back(fallback_location: root_path) 
+      flash[:alert] = @user.errors.full_messages.to_sentence
+    end  
+  end
+
+
 end
