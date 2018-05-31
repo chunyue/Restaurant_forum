c2c0a94ee7f293be390040834d9c22edd89faed4
liuchunyueh <liuchunyueh@gmail.com>
2018-06-01 00:19:24 +0800

    add user index page with follow button

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,5 @@
 class Followship < ApplicationRecord
   #屬於user 也屬於追蹤的人(following)
-  belong_to :user
-  belong_to :following, class_name: "User"
+  belongs_to :user
+  belongs_to :following, class_name: "User", optional: true
 end
