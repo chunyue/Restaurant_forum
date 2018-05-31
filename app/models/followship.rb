688b28660fe18d29585c96e62e26003774e0978d
liuchunyueh <liuchunyueh@gmail.com>
2018-06-01 00:20:20 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,6 @@
 class Followship < ApplicationRecord
+  validates :following_id, uniqueness: { scope: :user_id }
+
   #屬於user 也屬於追蹤的人(following)
   belongs_to :user
   belongs_to :following, class_name: "User", optional: true
